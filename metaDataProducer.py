c33ac48be9487e2230439c3eb6ab473103fa0b42
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2018-08-15 18:44:39 +0200

    added module to add branches with metadata based on sample name

---
+++ metaDataProducer.py
@@ -0,0 +1,80 @@
+import os
+import numpy as np
+import re
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+
+def idData(name):
+  if   'JetHT' in name: return 1
+  elif 'HTMHT' in name: return 2
+  elif 'MET' in name: return 3
+  elif 'DoubleEG' in name: return 4
+  elif 'DoubleMuon' in name: return 5
+  elif 'MuonEG' in name: return 6
+  elif 'SinglePhoton' in name: return 7
+  elif 'SingleMuon' in name: return 8
+  elif 'SingleElectron' in name: return 9
+
+class metaDataProducer(Module):
+  def __init__(self,xSecFile,sampleName,isMC):
+    self.xSecFile = xSecFile
+    self.sampleName = sampleName
+    self.isMC = isMC
+  def beginJob(self):
+    pass
+  def endJob(self):
+    pass
+
+  def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+    (id,xSec,filterEff,kFac) = (1,1,1,1)
+
+    if self.isMC:
+      if not os.path.isfile(self.xSecFile):
+       raise RuntimeError('cross-sections file is not available', self.xSecFile )
+      else:
+       f=open(self.xSecFile)
+       for line in f:
+         if '#' in line or line == '\n': continue # skip commented lines and empty lines
+         data = re.split(' |\t|\n', line) # split based on any space or tab - this should take all cases into account
+         data = filter(lambda x: x != '', data) # remove all empty elements from the list
+         sName = data[1]
+         if sName in self.sampleName: # if match, make assignment
+           (id,xSec,filterEff,kFac) = (int(data[0]),float(data[2]),float(data[3]),float(data[4]))
+           break # if you matched don't keep on looping
+
+    else: # data
+      id = idData(self.sampleName) # the others kept to 1
+
+    # initialize quantities to be filled in analyze
+    self.id = id
+    self.xSec = xSec
+    self.filterEff = filterEff
+    self.kFac = kFac
+
+    print 'Metadata for this sample: id={}, xSec={}, filterEff={}, kFac={}'.format(id,xSec,filterEff,kFac)
+
+    # create output branches with proper name, following previous convention
+    self.out = wrappedOutputTree
+    self.out.branch("evt_id", "I")
+    self.out.branch("evt_xsec", "F")
+    self.out.branch("evt_filter", "F")
+    self.out.branch("evt_kfactor", "F")
+
+  def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+    pass
+
+  def analyze(self, event):
+
+    self.out.fillBranch("evt_id", self.id)
+    self.out.fillBranch("evt_xsec", self.xSec)
+    self.out.fillBranch("evt_filter", self.filterEff)
+    self.out.fillBranch("evt_kfactor", self.kFac)
+
+    return True
+
+# define modules using the syntax 'name = lambda : constructor' to avoid having them loaded when not needed
+exampleSample = '/WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/NANOAODSIM'
+metaData = lambda : metaDataProducer(xSecFile='file.txt',sampleName=exampleSample,isMC=True)
--- postproc.py
+++ postproc.py
@@ -1,35 +1,25 @@
 #!/usr/bin/env python
-import os, sys
-import ROOT
-ROOT.PyConfig.IgnoreCommandLineOptions = True
-from importlib import import_module
-from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
+#Script to launch the post-processing of nanoAODs
 
 
-from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
-from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+def getSampleName(files, isMC):
+# this is ugly but it's not my fault
+# this sample name will be used to search for the cross-sections
 
-# you need to have at least one empty module to run
-# in principle it's better to use the way modules are passed in nano_postproc.py but it didn't work until now!
-class ExampleAnalysis(Module):
-  def __init__(self):
-    self.writeHistFile=True
+  first = files[0]
+  redirector = 'root://cms-xrd-global.cern.ch/'
 
-  def beginJob(self,histFile=None,histDirName=None):
-    #Module.beginJob(self,histFile,histDirName)
+  if first.startswith(redirector): first.replace(redirector, '')
+  elements = first.split('/')
+  elements = filter(lambda x: x != '', elements) # for safety reasons, remove empty strings
 
-    #self.h_vpt=ROOT.TH1F('sumpt',   'sumpt',   100, 0, 1000)
-    #self.addObject(self.h_vpt)
-    return True
-
-  def analyze(self, event):
-    return True
-
-#modules = [ExampleAnalysis()]
+  if isMC:
+    return elements[3]
+  else:
+    return elements[3] + '_' + elements[2]
 
 
 from argparse import ArgumentParser
-import os
 parser = ArgumentParser(description='', add_help=True)
 parser.add_argument('-o', '--outdir', type=str, dest='outdirname', help='name of the output dir', default='output/out')
 parser.add_argument('-N', '--Nevts',  type=int, dest='nevents', help='max events to run on', default=-1)
@@ -40,47 +30,21 @@ parser.add_argument('--doLocal', dest='doLocal', help='do local test, no crab in
 parser.add_argument('--doMC', dest='doMC', help='is it a monte carlo sample?', action='store_true', default=False)
 
 options = parser.parse_args()
-
-options.doMC = True
-options.year = '2017'
-
 print options
 
-from PhysicsTools.NanoAODTools.postprocessing.examples.mhtjuProducerCpp import mhtjuProducerCpp
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.lepSFProducer import lepSFProducer
-from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import btagSFProducer
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import puWeightProducer
-from PhysicsTools.NanoAODTools.postprocessing.analysis.mt2.mt2VarsProducer import mt2VarsProducer
 
-#modules = [mhtjuProducerCpp(), lepSFProducer('LooseWP_2016', 'GPMVA90_2016')]
-#modules = [mhtjuProducerCpp()]
-modules = [mt2VarsProducer(isMC=options.doMC, year=options.year)]
-            #lepSFProducer('LooseWP_2016', 'GPMVA90_2016'),
-            #btagSFProducer(era='2017', algo = 'csvv2'),
-            #puWeightProducer("auto","%s/src/PhysicsTools/NanoAODTools/python/postprocessing/data/pileup/PileupData_GoldenJSON_Full2016.root" % os.environ['CMSSW_BASE'],"pu_mc","pileup",verbose=False)]
-#modules = [ExampleAnalysis()]
-#from PhysicsTools.NanoAODTools.postprocessing.framework.exampleModule import exampleProducer
-#modules = [exampleProducer(jetSelection=lambda j : j.pt > 30)]
+import os, sys
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+from importlib import import_module
 
-#preselection='Jet_pt[0] > 250'
+
+## Preselection
+# FIXME: define a reasonable preselection based on quantites already present in the nanoAOD or defined after (?)
 preselection = ''
+sampleName = 'ABRACADABRA'
 
-#files = ['/scratch/mratti/MT2_test_nanoAODs/WJetsToLNu_HT-400To600__RunIIFall17NanoAOD/74D74976-1EFB-E711-A664-00269E95ACE4.root']
-# nanoAOD Run2017 PeriodD MET primary dataset, total of 4.319 (w/o normtag) / 4.224 ifb (w/ normtag)
-# full run list: 302030 303434
-#files = ['root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/2891A264-4C45-E811-A30A-C4346BC80410.root',
-#'root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/8CE082E9-4745-E811-ABAD-00266CFFCD00.root',
-#'root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/4061BBAC-5D45-E811-9EB3-00266CFFC7E0.root',
-#'root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/82944C1A-4945-E811-A345-001E67A400F0.root',
-#'root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/F69A061A-4745-E811-9E01-A4BF0101202F.root',
-#'root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/0A4E9DE6-4E45-E811-BDEC-EC0D9A0B30E0.root',
-#'root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/C87B3AEE-4A45-E811-B81C-90B11C1453E1.root',
-#'root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/D0B26618-4C45-E811-B98A-001E67DFFB31.root',
-#'root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/F6434365-4D45-E811-AE2C-001E67A4055F.root',
-#'root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/303698B0-5D45-E811-B34A-001E675A6928.root',
-#'root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/210000/D064F6E9-DC46-E811-AF6D-008CFAF28DCE.root']
-#files = ['/scratch/mratti/MT2_test_nanoAODs/ZJetsToNuNu_HT-600To800__RunIIFall17NanoAOD/A8548111-275A-E811-A7C4-A0369FC5E71C.root']
-#files=[' root://cms-xrd-global.cern.ch//store/mc/RunIISummer16NanoAOD/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NANOAODSIM/PUMoriond17_05Feb2018_94X_mcRun2_asymptotic_v2-v1/40000/2CE738F9-C212-E811-BD0E-EC0D9A8222CE.root']
+## Define input files here
 if options.doLocal:
   print 'Running in local'
   dofwkJobReport = False
@@ -89,17 +53,43 @@ if options.doLocal:
     #files = ['/shome/mratti/nanoaod_workarea/nano_making/CMSSW_9_4_6_patch1/src/PhysicsTools/NanoAOD/test/test94X_Wlv_NANO_15K.root']
     #files = ['/shome/mratti/nanoaod_workarea/nano_making/CMSSW_9_4_6_patch1/src/PhysicsTools/NanoAOD/test/test94X_Wlv_NANO_5K_V2.root']
     files = ['/shome/mratti/nanoaod_workarea/nano_making/CMSSW_9_4_6_patch1/src/PhysicsTools/NanoAOD/test/test94X_Wlv_NANO_5K_noselIT.root']
+    sampleName = 'WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8'
   elif options.what == 'Zll':
     #files = ['/shome/mratti/nanoaod_workarea/nano_making/CMSSW_9_4_6_patch1/src/PhysicsTools/NanoAOD/test/test94X_Zll_NANO_5K_V2.root']
     files = ['/shome/mratti/nanoaod_workarea/nano_making/CMSSW_9_4_6_patch1/src/PhysicsTools/NanoAOD/test/test94X_Zll_NANO_5K_nodxyIT.root']
+    sampleName = 'DYJetsToLL_M-50_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8'
+  elif options.what == 'data':
+    files = ['root://cms-xrd-global.cern.ch//store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/2891A264-4C45-E811-A30A-C4346BC80410.root']
+    sampleName = 'MET_Run2017D' # -31Mar2018-v1
 else:
   print 'Running on the grid'
   dofwkJobReport = True
   haddFileName = 'mt2.root'
   #this takes care of converting the input files from CRAB
   import PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper as CH # inputFiles,runsAndLumis
-  files = CH.inputFiles()
+  files = CH.inputFiles() # it is aweful that the input files are obtained in a such a confused way, but crab doesn't seem to support anything better than that!
+  #sampleName = CH.inputSampleName() # FIXME: must define the sampleName in this case
+  sampleName = getSampleName(files=files, isMC=isMC) # This is really poor, but CMS hasn't thought of any sample handler, which is a bit of a shame
+  #print sampleName
 
-p=PostProcessor(outputDir=options.outdirname,inputFiles=files,cut=preselection,branchsel='branchSel.txt', outputbranchsel='branchSel.txt',modules=modules,noOut=False, maxEvents=options.nevents, fwkJobReport=dofwkJobReport, haddFileName=haddFileName, provenance=True)
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
+from PhysicsTools.NanoAODTools.postprocessing.examples.mhtjuProducerCpp import mhtjuProducerCpp
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.lepSFProducer import lepSFProducer
+from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import btagSFProducer
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import puWeightProducer
+from PhysicsTools.NanoAODTools.postprocessing.analysis.mt2.mt2VarsProducer import mt2VarsProducer
+from PhysicsTools.NanoAODTools.postprocessing.analysis.mt2.metaDataProducer import metaDataProducer
+
+#modules = [mhtjuProducerCpp(), lepSFProducer('LooseWP_2016', 'GPMVA90_2016')]
+#modules = [mhtjuProducerCpp()]
+modules = [ mt2VarsProducer(isMC=options.doMC, year=options.year),
+            metaDataProducer(xSecFile='data/xSec/xSecs_2016.txt', sampleName=sampleName, isMC=options.doMC) ]
+            #lepSFProducer('LooseWP_2016', 'GPMVA90_2016'),
+            #btagSFProducer(era='2017', algo = 'csvv2'),
+            #puWeightProducer("auto","%s/src/PhysicsTools/NanoAODTools/python/postprocessing/data/pileup/PileupData_GoldenJSON_Full2016.root" % os.environ['CMSSW_BASE'],"pu_mc","pileup",verbose=False)]
+
+
+p=PostProcessor(outputDir=options.outdirname,inputFiles=files,cut=preselection,branchsel='data/branchSel/branchSel.txt', outputbranchsel='data/branchSel/branchSel.txt',
+                modules=modules,noOut=False, maxEvents=options.nevents, fwkJobReport=dofwkJobReport, haddFileName=haddFileName, provenance=True)
 
 p.run()
