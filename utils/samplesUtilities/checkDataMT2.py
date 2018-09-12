18c7347d59fd0c87d35035f1c3d7e1d04f218344
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2018-09-12 17:46:52 +0200

    trying to undestand how to handle the sample business

--- utils/samplesUtilities/checkDataMT2.py
+++ utils/samplesUtilities/checkDataMT2.py
@@ -2,13 +2,13 @@ import os
 
 if __name__ == "__main__":
 
-  dataFormat = 'NANOAOD' # NANOAOD MINIAOD
-  campaign = 'Run2017*31Mar2018*' # Run2016*05Feb2018* 
+  dataFormat = 'NANOAOD' # or 'NANOAOD'
+  campaign = 'Run2018*' # 22Aug2018 Run2017*31Mar2018*
   # https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookNanoAOD#Centrally_produced_samples
   # https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookMiniAOD
   #PDs = ['SinglePhoton', 'DoublePhoton', 'JetHT', 'HTMHT', 'MET', 'SingleElectron', 'SingleMuon', 'DoubleEG', 'DoubleMuon', 'MuonEG']
   PDs = ['MET']
   for PD in PDs:
     command = 'dasgoclient --query="dataset=/{pd}/{c}/{df}"'.format(pd=PD,c=campaign, df=dataFormat)
-    print '****\nSummary for:  PD =', PD,  ' campaign =' , campaign, ' datatier =', dataFormat, '\n****\n'
+    print '****\n# Summary for:  PD =', PD,  ' campaign =' , campaign, ' datatier =', dataFormat, '\n****\n'
     os.system(command)
