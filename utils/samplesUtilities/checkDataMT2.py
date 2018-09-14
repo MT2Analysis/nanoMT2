a5b8b241a3cedd5fb408675ffb5581ca26f075f3
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2018-09-14 20:54:28 +0200

    fixed creation of list

--- utils/samplesUtilities/checkDataMT2.py
+++ utils/samplesUtilities/checkDataMT2.py
@@ -3,12 +3,12 @@ import os
 if __name__ == "__main__":
 
   dataFormat = 'NANOAOD' # or 'NANOAOD'
-  campaign = 'Run2018*' # 22Aug2018 Run2017*31Mar2018*
+  campaign = 'Run2017*' # 22Aug2018 Run2017*31Mar2018*
   # https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookNanoAOD#Centrally_produced_samples
   # https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookMiniAOD
-  #PDs = ['SinglePhoton', 'DoublePhoton', 'JetHT', 'HTMHT', 'MET', 'SingleElectron', 'SingleMuon', 'DoubleEG', 'DoubleMuon', 'MuonEG']
-  PDs = ['MET']
+  PDs = ['SinglePhoton', 'DoublePhoton', 'JetHT', 'HTMHT', 'MET', 'SingleElectron', 'SingleMuon', 'DoubleEG', 'DoubleMuon', 'MuonEG']
+  #PDs = ['MET']
   for PD in PDs:
     command = 'dasgoclient --query="dataset=/{pd}/{c}/{df}"'.format(pd=PD,c=campaign, df=dataFormat)
-    print '****\n# Summary for:  PD =', PD,  ' campaign =' , campaign, ' datatier =', dataFormat, '\n****\n'
+    print '# ****\n# Summary for:  PD =', PD,  ' campaign =' , campaign, ' datatier =', dataFormat, '\n# ****\n'
     os.system(command)
