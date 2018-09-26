e21a379cc59d822f5bd4acd3903dd63538fe05e8
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2018-09-26 18:04:21 +0200

    utility to merge data and remove duplicates, README

---
+++ utils/merge/README.md
@@ -0,0 +1,78 @@
+# Merge
+Utility to merge the MT2 babytrees from crab into a single root file per sample, using ```haddnano.py```.
+
+The main class is ```GroupMerger```: it handles the merging and copy to a specified output directory. The merging and copying is done on a group of samples basis.
+
+A group of samples can be e.g. all the QCD samples and so on.
+The supported groups are specified in the launching script ```merge.py```.
+
+The handling of samples via groups makes the parallelisation easier, as well as improving the book-keeping of possibly missing samples to merge.
+
+The version of the output file will be characterised by two labels, the production label (PL) and the merging label (ML):
+- production label, version of nanoMT2, indicated with capital letters, e.g. V01_V03
+- merging label, version of merge, indicated with lower case, e.g. v2
+
+The merging is done sample-per-sample in a /tmp directory and then the merged file is copied to a /scratch/USER/merged_nanoMT2/${PL}_${ML}/<SAMPLE-NAME>
+
+### Configuration
+Add the list of samples that you want to do the merging for in ```cfg/```, one list for data, one list for MC.
+
+Everytime there is a new nanoMT2 production with production label PL, create (and commit!) a new sample list with the following naming convention:
+
+```
+data_<YEAR>_merge_<PL>.txt
+mc_bkg_<YEAR>_merge_<PL>.txt
+```
+
+### Run-time
+#### Testing
+```
+python merge.py -p TEST14 -v v1 -g Wlv -y 2017 --doMC
+python merge.py -p TEST14 -v v1 -g data -y 2017
+```
+
+#### Full production
+Data and MC all together:
+```
+python merge.py -p ${PL} -v ${ML} -g data -y 2017
+python merge.py -p ${PL} -v ${ML} -g all -y 2017 --doMC
+```
+
+##### Some recent examples
+Data:
+```
+python merge.py -p ${PL} -v v1 -y 2017 -g MET
+python merge.py -p ${PL} -v v1 -y 2017 -g JetHT
+python merge.py -p ${PL} -v v1 -y 2017 -g HTMHT # one period B is missing !
+python merge.py -p ${PL} -v v1 -y 2017 -g SingleElectron
+python merge.py -p ${PL} -v v1 -y 2017 -g SingleMuon
+python merge.py -p ${PL} -v v1 -y 2017 -g DoubleEG
+python merge.py -p ${PL} -v v1 -y 2017 -g DoubleMuon # three DEF periods are missing !
+#python merge.py -p ${PL} -v v1 -y 2017 -g MuonEG # files where not launched
+python merge.py -p ${PL} -v v1 -y 2017 -g SinglePhoton
+#python merge.py -p ${PL} -v v1 -y 2017 -g DoublePhoton
+```
+
+MC:
+```
+python merge.py -p ${PL} -v v1 -y 2017 --doMC -g Zvv
+python merge.py -p ${PL} -v v1 -y 2017 --doMC -g Wlv
+python merge.py -p ${PL} -v v1 -y 2017 --doMC -g Zll
+python merge.py -p ${PL} -v v1 -y 2017 --doMC -g Top
+python merge.py -p ${PL} -v v1 -y 2017 --doMC -g QCD
+python merge.py -p ${PL} -v v1 -y 2017 --doMC -g Gjets
+```
+
+
+#### Further treatment for data
+Since for data we are running on different primary datasets, it is important to remove the duplicated events across them.
+
+First, all PDs of the same period will be merged, then the duplicates will be removed
+
+The outputs will be stored in a /merged/ dir inside the original directory
+
+By activating --doForce, the merging and duplicate removal will happen even if not all samples are available
+
+```
+python data_merge_and_remove_duplicates.py -y 2017 -p ${PL} -v ${ML} --doForce
+```
---
+++ utils/merge/removeDuplicates.C
@@ -0,0 +1,213 @@
+/*
+Macro imported from MT2Analysis2015 - slightly modified to cope with nanomT2 output
+
+How to run:
+root -l
+.L removeDuplicates.C+
+removeDuplicates(inputfile.root, false, outputfile.root, treeName)
+
+Note: if you don't know whether there are duplicates, you should run on each baby-tree with the option 'false' as 2nd argument the first time. Only if duplicates will be found, run again with option 'true' to fill a new tree without duplicates. This will save time!
+*/
+
+
+#include "TTree.h"
+#include "TFile.h"
+#include "TFileCollection.h"
+#include "TChain.h"
+
+#include <iostream>
+#include <set>
+#include <ctime>
+
+using namespace std;
+
+class EventKey {
+public:
+  EventKey(unsigned int input_run=0, unsigned int input_lumi=0, unsigned long long input_evt=0) :
+    //EventKey(int input_run=0, int input_lumi=0, unsigned long long input_evt=0) :
+    run_(input_run), lumi_(input_lumi), evt_(input_evt){;}
+
+  unsigned int run() const {return run_;}
+  unsigned int lumi() const {return lumi_;}
+//  int run() const {return run_;}
+//  int lumi() const {return lumi_;}
+  unsigned long long evt() const {return evt_;}
+
+  bool operator<(EventKey const& right) const{
+    if (run_ == right.run()) {
+      if (lumi_ == right.lumi()) {
+	return evt_ < right.evt();
+      }
+      return lumi_ < right.lumi();
+    }
+    return run_ < right.run();
+  }
+
+private:
+  unsigned int run_;
+  unsigned int lumi_;
+//  int run_;
+//  int lumi_;
+  unsigned long long evt_;
+
+};
+
+
+
+void removeDuplicates(string inputFile="duplicates.root",
+		      bool fillNewTree=false,
+		      string outputFile="output.root",
+		      string treeName="mt2"){
+
+  int start_s=clock();
+
+  //Get input tree
+  TFile *oldfile = new TFile(inputFile.c_str());
+  TTree *oldtree = (TTree*)oldfile->Get(treeName.c_str());
+  Long64_t nentries = oldtree->GetEntries();
+
+  cout << "In input tree, nentries = " << nentries << endl;
+
+  unsigned int run,lumi;  // nanoAODs
+  //int run,lumi;         // americans
+  unsigned long long evt;
+  oldtree->SetBranchAddress("run",&run);
+  oldtree->SetBranchAddress("luminosityBlock",&lumi);
+  oldtree->SetBranchAddress("event",&evt);
+
+  //Create a new file + a clone of old tree in new file
+  TFile *newfile = new TFile(outputFile.c_str(),"recreate");
+  TTree *newtree = oldtree->CloneTree(0);
+
+
+  //Create set where we store list of event keys
+  std::set<EventKey> previousEvents;
+
+  int nDuplicates = 0;
+
+  for (Long64_t i=0;i<nentries; i++) {
+    //for (Long64_t i=0;i<1000; i++) {
+    oldtree->GetEntry(i);
+
+    EventKey newEvent(run,lumi,evt);
+    bool isDuplicate = !previousEvents.insert(newEvent).second;
+
+    if(i%100000==0) {
+      time_t t = time(0);   // get time now
+      tm * now = localtime( & t );
+      cout << "Processing event: " << i << " at time "
+	   << now->tm_hour << ":"
+	   << now->tm_min << ":"
+	   << now->tm_sec
+	   << endl;
+    }
+
+    if(!isDuplicate) {
+      if(fillNewTree) newtree->Fill();
+    }else{
+      nDuplicates++;
+      //cout << "Found duplicate! run,lumi,evt: "
+      //     << run << " , " << lumi << " , " << evt <<endl;
+    }
+
+  }
+
+  cout << "Number of duplicates found: " << nDuplicates << endl;
+
+
+  //newtree->Print();
+  newtree->AutoSave();
+  delete oldfile;
+  delete newfile;
+
+  int stop_s=clock();
+  cout << "elapsed time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << " seconds" << endl;
+}
+
+int removeDuplicatesFromChain(string inputFilesList,
+			      bool fillNewTree=false,
+			      string outputFile="output.root",
+			      string treeName="mt2"){
+  int start_s=clock();
+
+  // -------- Same code for TChain on remote files as in postProcessing.C
+  TChain* chain = new TChain(treeName.c_str());
+
+  //std::cout << inputFilesList.c_str() << std::endl;
+  TFileCollection *filelist = new TFileCollection("listOfFiles");
+
+  std::cout << "Adding files list... " << std::endl;
+  filelist->AddFromFile( inputFilesList.c_str() );
+
+  std::cout << "Adding files to chain... " << std::endl;
+  int chainReturn = chain->AddFileInfoList((TCollection*) filelist->GetList());
+
+  if (chainReturn < 1) {
+    std::cout << "ERROR: input folder/fileName is not well defined. Exit!" << std::endl;
+    std::cout << "InputFileList: " << inputFilesList << std::endl;
+    return 1;
+  }
+  // ------------------------------------------------------
+
+  cout << "Chain created" << endl;
+  Long64_t nentries = chain->GetEntries();
+
+  cout << "In input tree, nentries = " << nentries << endl;
+
+  unsigned int run,lumi;  //CMG
+  //int run,lumi;         // americans
+  unsigned long long evt;
+  chain->SetBranchAddress("run",&run);
+  chain->SetBranchAddress("lumi",&lumi);
+  chain->SetBranchAddress("evt",&evt);
+
+  //Create a new file + a clone of old tree in new file
+  TFile *newfile = new TFile(outputFile.c_str(),"recreate");
+  TTree *newtree = chain->CloneTree(0);
+
+  cout << "Tree cloned" << endl;
+
+  //Create set where we store list of event keys
+  std::set<EventKey> previousEvents;
+
+  int nDuplicates = 0;
+
+  for (Long64_t i=0;i<nentries; i++) {
+    chain->GetEntry(i);
+
+    EventKey newEvent(run,lumi,evt);
+    bool isDuplicate = !previousEvents.insert(newEvent).second;
+
+    if(i%100000==0) {
+      time_t t = time(0);   // get time now
+      tm * now = localtime( & t );
+      cout << "Processing event: " << i << " at time "
+	   << now->tm_hour << ":"
+	   << now->tm_min << ":"
+	   << now->tm_sec
+	   << endl;
+    }
+
+    if(!isDuplicate) {
+      if(fillNewTree) newtree->Fill();
+    }else{
+      nDuplicates++;
+      //cout << "Found duplicate! run,lumi,evt: "
+      //     << run << " , " << lumi << " , " << evt <<endl;
+    }
+
+  }
+
+  cout << "Number of duplicates found: " << nDuplicates << endl;
+
+
+  //newtree->Print();
+  newtree->AutoSave();
+  delete chain;
+  delete newfile;
+
+  int stop_s=clock();
+  cout << "elapsed time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << " seconds" << endl;
+
+  return 0;
+}
