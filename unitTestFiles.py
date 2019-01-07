a867f054a27cc4aaafe7ae8314251630def5e5b6
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2019-01-07 19:00:55 +0100

    test files more similar to previously

--- unitTestFiles.py
+++ unitTestFiles.py
@@ -8,10 +8,10 @@ f[2018] = {}
 # * 2016 *
 # ********
 # For Mo19
-f[2016]['data'] = '/store/data/Run2016C/MET/NANOAOD/Nano14Dec2018-v1/10000/75C4E995-2885-8048-BA07-FD4ACCD3572E.root'
-
+#f[2016]['data'] = '/store/data/Run2016C/MET/NANOAOD/Nano14Dec2018-v1/10000/75C4E995-2885-8048-BA07-FD4ACCD3572E.root'
+f[2016]['data'] = '/store/data/Run2016B_ver2/MET/NANOAOD/Nano14Dec2018_ver2-v1/80000/CD0F76D4-44C6-7742-A060-C75E23889C4A.root'
 # legacy
-f[2016]['data'] = '/store/data/Run2016B/MET/NANOAOD/22Aug2018_ver2-v1/10000/C83B52FF-34A9-E811-8D07-1CC1DE19286E.root'
+#f[2016]['data'] = '/store/data/Run2016B/MET/NANOAOD/22Aug2018_ver2-v1/10000/C83B52FF-34A9-E811-8D07-1CC1DE19286E.root'
 f[2016]['Wlv'] = '/store/mc/RunIISummer16NanoAODv3/WJetsToLNu_HT-600To800_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NANOAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v2/110000/1A7B787F-A7EA-E811-BAF6-549F35AD8BF0.root'
 f[2016]['QCD'] = '/store/mc/RunIISummer16NanoAODv3/QCD_HT300to500_BGenFilter_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NANOAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v1/100000/782BB8A0-97C7-E811-BA0F-001A648F1C52.root'
 f[2016]['sig'] = '/store/mc/RunIISummer16NanoAOD/SMS-T2tt_mStop-500_mLSP-325_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NANOAODSIM/PUMoriond17_05Feb2018_94X_mcRun2_asymptotic_v2-v1/00000/780DA7C8-2D16-E811-8455-0CC47A13CCFC.root'
@@ -22,9 +22,9 @@ f[2016]['sig'] = '/store/mc/RunIISummer16NanoAOD/SMS-T2tt_mStop-500_mLSP-325_Tun
 # * 2017 *
 # ********
 # For Mo19
-f[2017]['data'] = '/store/data/Run2017C/MET/NANOAOD/Nano14Dec2018-v1/00000/A70F9D32-0B09-5144-AAEA-D5E1BE2E0BEB.root'
+f[2017]['data'] = '/store/data/Run2017D/MET/NANOAOD/Nano14Dec2018-v1/10000/2536594C-ABC3-2A48-87DF-4FF2EDE55069.root'
 
-f[2017]['data'] = '/store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/2891A264-4C45-E811-A30A-C4346BC80410.root'
+#f[2017]['data'] = '/store/data/Run2017D/MET/NANOAOD/31Mar2018-v1/10000/2891A264-4C45-E811-A30A-C4346BC80410.root'
 f[2017]['Wlv'] = '/store/mc/RunIIFall17NanoAOD/WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/70000/5CD5289E-5856-E811-A5DB-A0369FD0B22A.root'
 
 # old PU
@@ -36,10 +36,10 @@ f[2017]['ttbar'] = '/store/mc/RunIIFall17NanoAOD/TTJets_TuneCP5_13TeV-amcatnloFX
 # * 2018 *
 # ********
 # For Mo19
-f[2018]['data'] = '/store/group/phys_susy/mratti/nanoMaking/NanoAODv4RePriv/MET/Run2018A-from_17Sep2018_ver1-NanoAODv4RePriv/181221_221716/0000/myNanoRunData2018ABC_NANO_321.root'
+f[2018]['data'] = '/store/group/phys_susy/mratti/nanoMaking/NanoAODv4RePriv/MET/Run2018C-from_17Sep2018_ver1-NanoAODv4RePriv/181221_221740/0000/myNanoRunData2018ABC_NANO_23.root'
 
 #
-f[2018]['data'] = '/store/data/Run2018C/MET/NANOAOD/14Sep2018_ver3-v1/60000/7F7F7AA4-FCBB-B343-BA4C-8678B7A1BAEC.root'
+#f[2018]['data'] = '/store/data/Run2018C/MET/NANOAOD/14Sep2018_ver3-v1/60000/7F7F7AA4-FCBB-B343-BA4C-8678B7A1BAEC.root'
 f[2018]['Wlv'] = '' # not MC currently
 
 
